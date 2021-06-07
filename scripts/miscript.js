39cf901e4e4fe7a59c2d1df973b68089fb96f116
Daniela Solis B <dasolis506@gmail.com>
2021-06-07 10:38:09 -0600

    tarea4

--- scripts/miscript.js
+++ scripts/miscript.js
@@ -1,14 +1,47 @@
-<script type="text/javascript">
 
 
-function sumar(a, b){
-    var x=document.getElementById(myNumber1).value;
-    var y=document.getElementById(myNumber2).value;
-    var sum=0;
-    sum=Number(x)+Number(y);
-    alert("SUM is: " + sum);
+function toggleBloque(){
+    var demo= document.getElementById("demo");
+    var operaciones= document.getElementById("operacionesContenedor");  
+    
+    if (operaciones.style.display === "none"){
+        demo.textContent = "Ocultar operaciones"
+        operaciones.style.display="block"
+
+    }else{
+        demo.textContent = "Mostrar operaciones"
+        operaciones.style.display="none"
+    }
+
 }
 
+function sumar(a, b){
+    var x=document.getElementById('myNumber1').valueAsNumber;
+    var y=document.getElementById('myNumber2').valueAsNumber;
+    var sum=0;
+    sum = x + y ;
+    imprimirResultado("suma",sum);
+}
+
+function restar(a, b){
+    var x=document.getElementById('myNumber1').valueAsNumber;
+    var y=document.getElementById('myNumber2').valueAsNumber;
+    var dif=0;
+    dif = x - y ;
+    imprimirResultado("resta",dif);
+
+}function multiplicar(a, b){
+    var x=document.getElementById('myNumber1').valueAsNumber;
+    var y=document.getElementById('myNumber2').valueAsNumber;
+    var prod=0;
+    prod = x * y ;
+    imprimirResultado("multiplicacion",prod);
+}
+
+
+  function imprimirResultado(operacion, resultado){
+       document.getElementById('result').textContent = "La " + operacion + " es " + resultado;
+    }
 
 function validarVacios(resultado,operacion) {
     if(!isNaN(resultado)) {
@@ -17,8 +50,3 @@ function validarVacios(resultado,operacion) {
         document.getElementById('result').textContent = "Debe ingresar valores numericos";
     }
 }
-
-
-
-</script>
-
